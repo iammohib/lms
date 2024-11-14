c97318620abb84d5d0cc92a0aa67e51a86be98c7
Mohd Mohibuddin <swe.mohib@gmail.com>
2024-11-14 22:15:03 +0530

    feat: added page not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import AboutUs from "./pages/AboutUs";
 import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
+
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
